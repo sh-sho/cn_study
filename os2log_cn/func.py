71692301fe3ddc7b36325a643b718058fc83094b
Oracle Public Cloud User <opc@cn-study-o.sub06220547180.vcn1.oraclevcn.com>
2023-09-07 07:57:49 +0000

    commit cn_study

---
+++ os2log_cn/func.py
@@ -0,0 +1,46 @@
+import io
+import os
+import json
+import sys
+from fdk import response
+
+import oci.object_storage
+
+def handler(ctx, data: io.BytesIO=None):
+    signer = oci.auth.signers.get_resource_principals_signer()
+
+    try:
+        body = json.loads(data.getvalue())
+        bucketName = body["bucketName"]
+        fileName = body["fileName"]
+
+    except Exception as e:
+        error = 'Input a JSON object in the format: \'{"bucketName": "<bucket name>"}, "fileName": "<filename>"}\' '
+        raise Exception(error)
+
+    resp = do(signer, bucketName, fileName)
+
+    return response.Response(
+        ctx, response_data=json.dumps(resp),
+        headers={"Content-Type": "application/json"}
+    )
+
+def do(signer, bucketName, fileName):
+    client = oci.object_storage.ObjectStorageClient(config={}, signer=signer)
+    message = "Failed: The object " + str(fileName) + " could not be retrieved."
+
+    try:
+        print("Searching for bucket and object", file=sys.stderr)
+        object = client.get_object(os.environ.get("OCI_NAMESPACE"), bucketName, fileName)
+
+        print("found object", file=sys.stderr)
+        if object.status == 200:
+            message = "Success: The object " + str(fileName) + " was retrieved, content: " + str(object.data.text)
+
+    except Exception as e:
+        message = "Failed: " + str(e.message)
+
+    response = {
+        "content": message
+    }
+    return response
